dbdfc2a8f9e2dba1a22771e1760b580d9be62d87
Matthew Young <matthew.young@unh.edu>
2021-12-20 12:16:48 -0500

    Rename variables and expand docstring

--- src/goats/common/numerical.py
+++ src/goats/common/numerical.py
@@ -27,34 +27,53 @@ def find_1d_indices(
 
 _NT = TypeVar('_NT', bound=numbers.Complex)
 def find_nearest(
-    array: Sequence[_NT],
-    value: _NT,
+    values: Iterable[_NT],
+    target: _NT,
     bound: str=None,
 ) -> Tuple[int, _NT]:
-    """Find the array value nearest the target value.
+    """Find the sequence value nearest the target value.
     
-    Returns a 2-tuple whose first element is the array index of the nearest value and second element is the nearest value.
+    Returns a 2-tuple whose first element is the sequence index of the nearest
+    value and second element is the nearest value.
 
-    This function is based on the top answer to this StackOverflow question: 
+    Parameters
+    ----------
+    values : iterable of numbers
+        An iterable collection of numbers to compare to the target value. Must
+        support conversion to a `numpy.ndarray`.
+
+    target : number
+        A single numerical value for which to search in `values`. Must be
+        coercible to the type of `values`.
+
+    bound : {None, 'lower', 'upper'}
+        The constraint to apply when finding the nearest value::
+        - None: no constraint
+        - 'lower': ensure that the nearest value is equal to or greater than the
+        target value
+        - 'upper': ensure that the nearest value is equal to or less than the
+        target value
+
+    Notes
+    -----
+    This function is based on the top answer to this StackOverflow question:
     https://stackoverflow.com/questions/2566412/find-nearest-value-in-numpy-array
-    However, a lower-voted answer (and the comments) has some suggestions for a bisection-based method.
+    However, a lower-voted answer (and the comments) has some suggestions for a
+    bisection-based method.
 
-    Pass `bound='lower'` to ensure that the nearest value is equal to or greater than the target value.
-    Pass `bound='upper'` to ensure that the nearest value is equal to or less than the target value.
     """
 
-    array = np.asarray(array)
-    array = np.squeeze(array)
-    index = np.abs(array - value).argmin()
+    array = np.asarray(values).squeeze()
+    index = np.abs(array - target).argmin()
     if bound == 'lower':
         try:
-            while array[index] < value:
+            while array[index] < target:
                 index += 1
         except IndexError:
             index = -1
     elif bound == 'upper':
         try:
-            while array[index] > value:
+            while array[index] > target:
                 index -= 1
         except IndexError:
             index = 0
