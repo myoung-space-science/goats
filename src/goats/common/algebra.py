5f2ad99562b7b6e66a2b6518ef7cffe4b6fa5e49
Matthew Young <matthew.young@unh.edu>
2022-01-06 18:21:38 -0600

    Extract methods to simplify Parser._parse_operand

--- src/goats/common/algebra.py
+++ src/goats/common/algebra.py
@@ -964,35 +964,66 @@ class Parser:
         current = Iteration(initial.base)
         while current.string:
             previous = current.copy()
-            if parsed := self.operators.parse(current.string):
-                current.operator = parsed.result
-                if exception := self._operator_error(
-                    current.operator,
-                    previous.operator,
-                ): raise exception(initial)
-                current.string = parsed.remainder
-            if parsed := self.operands.parse(current.string):
-                current.operand = parsed.result ** initial.exponent
-                current.string = parsed.remainder
-            if current.string == previous.string:
-                if current.string == initial.base:
-                    raise ParsingValueError(
-                        f"Failed to parse {current.string!r}"
-                    )
-                raise ParsingValueError(
-                    "Failed to find a match for"
-                    f" {current.string!r} in {initial.base!r}"
-                )
-            if current.operand:
-                if current.operator:
-                    evaluated = self._evaluate(
+            current = self._get_operator(initial, current, previous)
+            current = self._get_operand(initial, current)
+            self._check_progress(initial, current, previous)
+            if new := self._compute_operand(current):
+                operands.append(new)
+        return operands
+
+    def _compute_operand(self, current: Iteration):
+        """Create a new operand from the current iteration."""
+        if current.operand:
+            if current.operator:
+                return self._evaluate(
                         current.operator,
                         current.operand,
                     )
-                    operands.append(evaluated)
-                else:
-                    operands.append(current.operand)
-        return operands
+            return current.operand
+
+    def _check_progress(
+        self,
+        initial: Operand,
+        current: Iteration,
+        previous: Iteration,
+    ) -> None:
+        """Raise an error if the string has not changed."""
+        if current.string == previous.string:
+            if current.string == initial.base:
+                raise ParsingValueError(
+                        f"Failed to parse {current.string!r}"
+                    )
+            raise ParsingValueError(
+                    "Failed to find a match for"
+                    f" {current.string!r} in {initial.base!r}"
+                )
+
+    def _get_operator(
+        self,
+        initial: Operand,
+        current: Iteration,
+        previous: Iteration,
+    ) -> Iteration:
+        """Attempt to parse an operator from the current string."""
+        if parsed := self.operators.parse(current.string):
+            current.operator = parsed.result
+            if exception := self._operator_error(
+                    current.operator,
+                    previous.operator,
+                ): raise exception(initial)
+            current.string = parsed.remainder
+        return current
+
+    def _get_operand(
+        self,
+        initial: Operand,
+        current: Iteration,
+    ) -> Iteration:
+        """Attempt to parse an operand from the current string."""
+        if parsed := self.operands.parse(current.string):
+            current.operand = parsed.result ** initial.exponent
+            current.string = parsed.remainder
+        return current
 
     def _operator_error(self, current: Operator, previous: Operator):
         """Check for known operator-related errors.
