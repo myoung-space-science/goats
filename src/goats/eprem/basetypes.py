02ef95d3b8c695005df752ee7e22a384d33c83b3
Matthew Young <matthew.young@unh.edu>
2022-01-07 13:53:48 -0600

    Overhaul module:
    * Redefine basetypes metadata
    * Define BaseTypes mapping, which reads from file
    * Create test module to check values

--- src/goats/eprem/basetypes.py
+++ src/goats/eprem/basetypes.py
@@ -1,191 +1,272 @@
 """
 Constants defined in EPREM `src/baseTypes.h`.
 """
-import numpy as np
+import pathlib
+import numbers
+import typing
 
+from goats.common import algebra
 from goats.common import constants
+from goats.common import iotools
 from goats.common import iterables
+from goats.common import numerical
 
 
-_primary = {
+_BASETYPES_H = {
+    'T': {
+        'info': 'True',
+        'type': int,
+    },
+    'F': {
+        'info': 'False',
+        'type': int,
+    },
     'PI': {
         'info': 'The value of π.',
-        'unit': '1',
-        'value': 3.1415926535897932,
+        'type': float,
     },
     'TWO_PI': {
         'info': 'The value of 2π.',
-        'unit': '1',
-        'value': 6.283185307179586,
+        'type': float,
     },
     'VERYSMALL': {
         'info': 'A very small value.',
-        'unit': '1',
-        'value': 1.0e-33,
+        'type': float,
     },
     'BADVALUE': {
         'info': 'A bad (invalid) float value.',
-        'unit': '1',
-        'value': 1.0e33,
+        'type': float,
     },
     'BADINT': {
         'info': 'A bad (invalid) integer value.',
-        'unit': '1',
-        'value': 2147483647,
+        'type': int,
     },
     'MP': {
-        'info': 'The value of the proton mass.',
+        'info': 'The proton mass.',
         'unit': 'g',
-        'value': 1.6726e-24,
+        'type': float,
     },
     'EV': {
         'info': 'The conversion from eVs to ergs.',
         'unit': 'erg/eV',
-        'value': 1.6022e-12,
+        'type': float,
     },
     'MEV': {
         'info': 'The conversion from MeVs to ergs.',
         'unit': 'erg/MeV',
-        'value': 1.6022e-6,
+        'type': float,
     },
     'GEV': {
         'info': 'The conversion from GeVs to ergs.',
         'unit': 'erg/GeV',
-        'value': 1.6022e-3,
+        'type': float,
     },
     'Q': {
-        'info': 'The value of the proton charge.',
-        'unit': 'statcoul',
-        'value': 4.80320425e-10,
+        'info': 'The proton charge.',
+        'unit': 'statC',
+        'type': float,
     },
     'C': {
-        'info': 'The value of the speed of light.',
+        'info': 'The speed of light.',
         'unit': 'cm/s',
-        'value': 2.99792458e10,
+        'type': float,
+    },
+    'MZERO': {
+        'info': 'The proton rest-mass energy in GeV.',
+        'unit': 'GeV',
+        'formula': 'MP * C^2 * GEV^-1',
+        'type': float,
     },
     'AU': {
-        'info': 'The value of 1 au.',
+        'info': 'One astronomical unit.',
         'unit': 'cm',
-        'value': 1.495978707e13,
+        'type': float,
     },
     'RSUN': {
         'info': 'The value of the solar radius.',
         'unit': 'cm',
-        'value': 6.96e10,
+        'type': float,
+    },
+    'RSAU': {
+        'info': 'The number of solar radii per au.',
+        'formula': 'RSUN * AU^-1',
+        'type': float,
+    },
+    'TAU': {
+        'info': 'The canonical EPREM time scale.',
+        'unit': 's',
+        'formula': 'AU * C^-1',
+        'type': float,
+    },
+    'DAY': {
+        'info': 'The conversion from EPREM time steps to Julian days.',
+        'unit': 'day',
+        'formula': 'TAU * 1.1574074074074072e-05', 
+        'type': float,
     },
     'MHD_DENSITY_NORM': {
         'info': 'The normalization factor for density.',
-        'unit': '1',
-        'value': 1.0,
-    },
-    'VOLT': {
-        'info': 'The conversion from volts to statvolts.',
-        'unit': 'statvolt/V',
-        'value': 0.33333e-2,
-    },
-    'THRESH': {
-        'info': 'The threshold for perpendicular diffusion.',
-        'unit': '1',
-        'value': 0.025,
-    },
-    'MAS_TIME_NORM': {
-        'info': 'The MAS time normalization.',
-        'unit': '1',
-        'value': 1445.87003080685,
-    },
-    'MAS_LENGTH_NORM': {
-        'info': 'The MAS length normalization.',
-        'unit': '1',
-        'value': 6.96e10,
-    },
-    'MAS_RHO_NORM': {
-        'info': 'The MAS plasma-density normalization.',
-        'unit': '1',
-        'value': 1.6726e-16,
-    },
-    'NUM_FACES': {
-        'info': 'The number of faces in the EPREM logical grid.',
-        'unit': '1',
-        'value': 6,
-    },
-}
-
-b = {k: v['value'] for k, v in _primary.items()}
-_derived = {
-    'MZERO': {
-        'info': 'The proton rest-mass energy in GeV.',
-        'unit': 'GeV',
-        'value': b['MP'] * b['C']**2 / b['GEV'],
-    },
-    'RSAU': {
-        'info': 'The solar radius in au.',
-        'unit': 'Rs/au',
-        'value': b['RSUN'] / b['AU'],
-    },
-    'TAU': {
-        'info': 'The EPREM time scale in seconds.',
-        'unit': 's',
-        'value': b['AU'] / b['C'],
-    },
-}
-d = {k: v['value'] for k, v in _derived.items()}
-_derived.update({
-    'DAY': {
-        'info': 'The conversion from EPREM time steps to Julian days.',
-        'unit': 'days',
-        'value': d['TAU'] / (24.0 * 60.0 * 60.0),
+        'type': float,
     },
     'MHD_B_NORM': {
         'info': 'The normalization for magnetic fields.',
-        'unit': '1',
-        'value': np.sqrt(b['MP'] * b['MHD_DENSITY_NORM'])* b['C'],
+        'formula': 'MP^0.5 * MHD_DENSITY_NORM^0.5 * C',
+        'type': float,
     },
-})
-d = {k: v['value'] for k, v in _derived.items()}
-_derived.update({
     'OM': {
         'info': 'The normalization for ion gyrofrequency.',
-        'unit': '1',
-        'value': (
-            d['MHD_B_NORM'] * b['Q'] * b['AU'] / (b['MP'] * b['C']**2)
-        ),
+        'formula': 'MHD_B_NORM * Q * AU * MP^-1 * C^-2',
+        'type': float,
     },
     'FCONVERT': {
         'info': 'The conversion from distribution to flux.',
-        'unit': '1',
-        'value': 1.0e30 / b['C']**4,
+        'formula': 'C^-4 * 1e+30',
+        'type': float,
+    },
+    'VOLT': {
+        'info': 'The conversion from volts to statvolts.',
+        'type': float,
+    },
+    'THRESH': {
+        'info': 'The threshold for perpendicular diffusion.',
+        'type': float,
+    },
+    'MAS_TIME_NORM': {
+        'info': 'The MAS time normalization factor.',
+        'type': float,
+    },
+    'MAS_LENGTH_NORM': {
+        'info': 'The MAS length normalization factor.',
+        'type': float,
+    },
+    'MAS_RHO_NORM': {
+        'info': 'The MAS plasma-density normalization.',
+        'type': float,
     },
     'MAS_TIME_CONVERT': {
-        'info': 'The time conversion from MAS unit.',
-        'unit': '1',
-        'value': b['MAS_TIME_NORM'] / (24.0 * 60.0 * 60.0),
+        'info': 'The time conversion from MAS units.',
+        'formula': 'MAS_TIME_NORM * 1.1574074074074072e-05',
+        'type': float,
     },
     'MAS_V_CONVERT': {
-        'info': 'The velocity conversion from MAS unit.',
-        'unit': '1',
-        'value': (
-            (b['MAS_LENGTH_NORM'] / b['MAS_TIME_NORM']) / b['C']
-        ),
+        'info': 'The velocity conversion from MAS units.',
+        'formula': 'MAS_LENGTH_NORM * MAS_TIME_NORM^-1 * C^-1',
+        'type': float,
     },
     'MAS_RHO_CONVERT': {
-        'info': 'The density conversion from MAS unit.',
-        'unit': '1',
-        'value': (b['MAS_RHO_NORM'] / b['MP']) / b['MHD_DENSITY_NORM'],
+        'info': 'The density conversion from MAS units.',
+        'formula': 'MAS_RHO_NORM * MP^-1 * MHD_DENSITY_NORM^-1',
+        'type': float,
     },
     'MAS_B_CONVERT': {
-        'info': 'The magnetic field conversion from MAS unit.',
-        'unit': '1',
-        'value': (
-            np.sqrt(4.0 * np.pi * b['MAS_RHO_NORM'])
-            * b['MAS_LENGTH_NORM'] / b['MAS_TIME_NORM']
-            / d['MHD_B_NORM']
+        'info': 'The magnetic field conversion from MAS units.',
+        'formula': (
+            'PI^0.5 * MAS_RHO_NORM^0.5 * MAS_LENGTH_NORM * MAS_TIME_NORM^-1 '
+            '* MHD_B_NORM^-1 * 2.0'
         ),
+        'type': float,
     },
-})
+    'MAX_STRING_SIZE': {
+        'type': int,
+    },
+    'MHD_DEFAULT': {
+        'info': 'Use the default MHD solver.',
+        'type': int,
+    },
+    'MHD_ENLIL': {
+        'info': 'Use ENLIL for MHD values.',
+        'type': int,
+    },
+    'MHD_LFMH': {
+        'info': 'Use LFM for MHD values.',
+        'type': int,
+    },
+    'MHD_BATSRUS': {
+        'info': 'Use BATS-R-US for MHD values.',
+        'type': int,
+    },
+    'MHD_MAS': {
+        'info': 'Use MAS for MHD values.',
+        'type': int,
+    },
+    'NUM_MPI_BOUNDARY_FLDS': {
+        'info': 'Number of MPI psuedo-fields for use in creating MPI typedefs.',
+        'type': int,
+    },
+}
 
-metadata = {**_primary, **_derived}
-"""Metadata for all base-type constants defined here."""
 
+class BaseTypes(iterables.MappingBase):
+    """Constants defined in EPREM `src/baseTypes.h`."""
 
-collection = iterables.UniformMapping(metadata, constants.Constant)
+    def __init__(self, src: typing.Union[str, pathlib.Path]=None) -> None:
+        self.path = iotools.ReadOnlyPath(src or '.') / 'baseTypes.h'
+        self._definitions = None
+        self._mapping = {k: self._evaluate(k) for k in self.definitions}
+        super().__init__(self._mapping)
+
+    def __getitem__(self, name: str):
+        """Access constants by name."""
+        if name in self._mapping:
+            value = self._mapping[name]
+            reference = _BASETYPES_H.get(name)
+            unit = reference.get('unit')
+            info = reference.get('info')
+            return constants.Constant(value, unit=unit, info=info)
+        raise KeyError(f"No {name!r} in baseTypes.h")
+
+    @property
+    def definitions(self):
+        """The definition of each constant in the simulation source code."""
+        if self._definitions is None:
+            with self.path.open('r') as fp:
+                lines = fp.readlines()
+            definitions = {}
+            for line in lines:
+                if line.startswith('#define'):
+                    parts = line.strip('#define').rstrip('\n').split(maxsplit=1)
+                    if len(parts) == 2:
+                        key, value = parts
+                        metadata = _BASETYPES_H.get(key, {})
+                        if 'formula' in metadata:
+                            definitions[key] = algebra.Expression(value)
+                        else:
+                            cast = metadata.get('type', str)
+                            definitions[key] = cast(value)
+            self._definitions = definitions
+        return self._definitions
+
+    def _evaluate(self, key: str) -> numbers.Real:
+        """Compute the value of a defined constant."""
+        target = self.definitions[key]
+        if isinstance(target, numbers.Real):
+            return target
+        if isinstance(target, algebra.Expression):
+            value = 1.0
+            for term in target:
+                if term.base in self.definitions:
+                    value *= float(term(self._evaluate(term.base)))
+                elif term.base == '1':
+                    value *= float(term)
+            return value
+        raise TypeError(target)
+
+    def print(self, tabsize: int=4, stream: typing.TextIO=None):
+        """Print formatted reference information."""
+        indent = ' ' * tabsize
+        print("{", file=stream)
+        for key, definition in self.definitions.items():
+            formula = (
+                definition.format(separator=' * ')
+                if isinstance(definition, algebra.Expression)
+                else None
+            )
+            print(
+                f"{indent}{key!r}: {'{'}\n"
+                f"{indent}{indent}'value': {self[key]},\n"
+                f"{indent}{indent}'formula': {formula!r},\n"
+                f"{indent}{'},'}",
+                file=stream,
+            )
+        print("}", file=stream)
 
---
+++ tests/eprem/test_basetypes.py
@@ -0,0 +1,50 @@
+from goats.eprem import basetypes
+
+
+reference = {
+    'T': 1,
+    'F': 0,
+    'PI': 3.141592653589793,
+    'TWO_PI': 6.283185307179586,
+    'VERYSMALL': 1e-33,
+    'BADVALUE': 1e+33,
+    'BADINT': 2147483647,
+    'MP': 1.6726e-24,
+    'EV': 1.6022e-12,
+    'MEV': 1.6022e-06,
+    'GEV': 0.0016022,
+    'Q': 4.80320425e-10,
+    'C': 29979245800.0,
+    'MZERO': 0.9382461065754594,
+    'AU': 14959787070000.0,
+    'RSUN': 69600000000.0,
+    'RSAU': 0.0046524726370988385,
+    'TAU': 499.0047838361564,
+    'DAY': 0.005775518331436994,
+    'MHD_DENSITY_NORM': 1.0,
+    'MHD_B_NORM': 0.038771870111656996,
+    'OM': 185327.43617160583,
+    'FCONVERT': 1.2379901472361203e-12,
+    'VOLT': 0.0033333,
+    'THRESH': 0.025,
+    'MAS_TIME_NORM': 1445.87003080685,
+    'MAS_LENGTH_NORM': 69600000000.0,
+    'MAS_RHO_NORM': 1.6726e-16,
+    'MAS_TIME_CONVERT': 0.016734606838042242,
+    'MAS_V_CONVERT': 0.0016056810454004597,
+    'MAS_RHO_CONVERT': 100000000.00000001,
+    'MAS_B_CONVERT': 56.9199110449208,
+    'MAX_STRING_SIZE': 240,
+    'MHD_DEFAULT': 0,
+    'MHD_ENLIL': 1,
+    'MHD_LFMH': 2,
+    'MHD_BATSRUS': 3,
+    'MHD_MAS': 4,
+    'NUM_MPI_BOUNDARY_FLDS': 2,
+}
+
+def test_all():
+    """Regression test for values."""
+    b = basetypes.BaseTypes('~/emmrem/epicMas/source/eprem/src')
+    for key, value in reference.items():
+        assert b[key].value == value
