28d4d8f5d6cfdccc3b333be4b60f253b6aac312e
Matthew Young <matthew.young@unh.edu>
2022-01-11 10:10:03 -0600

    Create conftest.py for eprem tests

---
+++ tests/eprem/conftest.py
@@ -0,0 +1,17 @@
+import pathlib
+
+import pytest
+
+
+@pytest.fixture
+def rootpath() -> pathlib.Path:
+    """The root path to test data.
+
+    This function gets the current working directory (`cwd`) from the resolved
+    file path rather than from `pathlib.Path().cwd()` because the latter
+    returns the current working directory of the caller.
+    """
+    cwd = pathlib.Path(__file__).expanduser().resolve().parent
+    pkgpath = cwd.parent.parent
+    return pkgpath / 'data' / 'eprem'
+
--- tests/eprem/test_datasets.py
+++ tests/eprem/test_datasets.py
@@ -7,23 +7,10 @@ from goats.common import indexing
 from goats.eprem import datasets
 
 
-def get_rootpath() -> Path:
-    """The root path to test data.
-
-    This function gets the current working directory (`cwd`) from the resolved
-    file path rather than from `pathlib.Path().cwd()` because the latter
-    returns the current working directory of the caller.
-    """
-    cwd = Path(__file__).expanduser().resolve().parent
-    pkgpath = cwd.parent.parent
-    return pkgpath / 'data' / 'eprem'
-
-
 @pytest.fixture
-def dataset():
+def dataset(rootpath: Path):
     """An EPREM dataset."""
-    dataroot = get_rootpath()
-    datapath = dataroot / 'cone' / 'obs' / 'obs000000.nc'
+    datapath = rootpath / 'cone' / 'obs' / 'obs000000.nc'
     return datasets.DatasetView(datapath, 'mks')
 
 
