b7f6a60cca4b89ea1d927c827024225320ee8a64
Matthew Young <matthew.young@unh.edu>
2022-02-09 15:55:08 -0500

    Create Restriction class

--- src/goats/eprem/interpolation.py
+++ src/goats/eprem/interpolation.py
@@ -1,6 +1,7 @@
 import typing
 
 import numpy as np
+import numpy.typing
 from scipy.interpolate import interp1d
 
 from goats.common.numerical import get_bounding_indices
@@ -12,6 +13,56 @@ def restrict_shells(r: np.ndarray, r0: float):
     lower = min(bounds[:, 0])
     upper = max(bounds[:, 1])
     return range(lower, upper+1)
+
+
+class Restriction:
+    """A subset of indices with which to create subsets of arrays."""
+
+    def __init__(
+        self,
+        rule: typing.Callable[..., typing.Iterable[typing.SupportsIndex]],
+        *args,
+        **kwargs,
+    ) -> None:
+        """
+        Create a new restriction.
+
+        Parameters
+        ----------
+        rule : callable
+            The rule by which to create a subset of array indices. This must be
+            a callable object that returns an iterable of objects to be used as
+            indices (e.g., `range`).
+
+        *args
+            Positional arguments to pass to `rule`.
+
+        **kwargs
+            Keyword arguments to pass to `rule`.
+        """
+        self.indices = rule(*args, **kwargs)
+
+    def apply(self, array: numpy.typing.ArrayLike, axis: int=None):
+        """Restrict `array` to a subset along `axis`.
+        
+        Parameters
+        ----------
+        array : array-like
+            The array from which to extract a subarray corresponding to the
+            restricted indices.
+
+        axis : int, default=-1
+            The axis of `array` to restrict.
+
+        Returns
+        -------
+        array-like
+            The subarray.
+        """
+        axis = axis or -1
+        permuted = np.moveaxis(array, axis, 0)
+        result = permuted[self.indices, ...]
+        return np.moveaxis(result, 0, axis)
 
 
 def radius(
