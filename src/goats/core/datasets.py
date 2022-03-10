bed2052e6e995f228e0f808c4d56f0a12e7db35a
Matthew Young <matthew.young@unh.edu>
2022-03-10 13:31:19 -0500

    Add observables to dict

--- src/goats/core/datasets.py
+++ src/goats/core/datasets.py
@@ -284,6 +284,66 @@ _OBSERVABLES = {
             'number / (area * solid_angle * time * energy / mass_number)'
         ),
     },
+    ('x', 'X'): {
+        'quantity': 'length',
+    },
+    ('y', 'Y'): {
+        'quantity': 'length',
+    },
+    ('z', 'Z'): {
+        'quantity': 'length',
+    },
+    ('b_mag', '|B|', 'B', 'bmag', 'b mag'): {
+        'quantity': 'magnetic field',
+    },
+    ('v_mag', '|V|', 'V', 'vmag', 'v mag'): {
+        'quantity': 'velocity',
+    },
+    ('bv_mag', 'bv', '|bv|', 'BV', '|BV|'): {
+        'quantity': 'velocity * magnetic field',
+    },
+    ('v_para', 'vpara', 'Vpara'): {
+        'quantity': 'velocity',
+    },
+    ('v_perp', 'vperp', 'Vperp'): {
+        'quantity': 'velocity',
+    },
+    ('flow_angle', 'flow angle', 'angle'): {
+        'quantity': 'plane angle',
+    },
+    ('div_v', 'divV', 'divv', 'div V', 'div v', 'div(V)', 'div(v)'): {
+        'quantity': '1 / time',
+    },
+    ('density_ratio', 'density ratio' ,'n2/n1', 'n_2/n_1'): {
+        'quantity': 'number',
+    },
+    ('rigidity', 'Rg', 'R_g'): {
+        'quantity': 'momentum / charge',
+    },
+    ('mean_free_path', 'mean free path', 'mfp'): {
+        'quantity': 'length',
+    },
+    ('acceleration_rate', 'acceleration rate'): {
+        'quantity': '1 / time',
+    },
+    ('energy_density', 'energy density'): {
+        'quantity': 'energy / volume',
+    },
+    ('average_energy', 'average energy'): {
+        'quantity': 'energy',
+    },
+    ('isotropic_distribution', 'isotropic distribution', 'isodist', 'f'): {
+        'removed axes': ['mu'],
+        'quantity': 'particle distribution',
+    },
+    'fluence': {
+        'removed axes': ['time'],
+        'quantity': 'number / (area * solid_angle * energy / mass_number)',
+    },
+    ('integral_flux', 'integral flux'): {
+        'removed axes': ['energy'],
+        'quantity': 'number / (area * solid_angle * time)',
+    },
 }
 
 
