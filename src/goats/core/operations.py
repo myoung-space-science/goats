5430b7e7a0792ebe6eb7c3c5ea1e1ba85855f750
Matthew Young <matthew.young@unh.edu>
2022-06-01 13:03:13 -0400

    Fix bug in Rules.__getitem__

--- src/goats/core/operations.py
+++ src/goats/core/operations.py
@@ -497,8 +497,11 @@ class Rules(_RulesType):
         for t in self.mapping:
             if all(issubclass(i, j) for i, j in zip(types, t)):
                 return self._from(t)
-        if self._type in types or any(issubclass(t, self._type) for t in types):
-            return self.implicit
+        if (
+            self._type is not None
+            and (self._type in types
+            or any(issubclass(t, self._type) for t in types))
+        ): return self.implicit
         raise KeyError(f"No rule for operand type(s) {__k!r}") from None
 
     def _from(self, types: Types):
