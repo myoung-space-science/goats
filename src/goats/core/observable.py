9297ed08adea96d325153b8f59ff419de10185d4
Matthew Young <matthew.young@unh.edu>
2022-07-20 12:01:08 -0400

    Begin defining core objects (WIP): * Implementation ABC * Primary implementation * Derived implementation * Compound implementation * Quantity * Interface ABC

--- src/goats/core/observable.py
+++ src/goats/core/observable.py
@@ -1,23 +1,88 @@
 import abc
-import math
-import operator as standard
+import collections.abc
 import typing
 
 from goats.core import algebraic
-from goats.core import datatypes
-from goats.core import iterables
-from goats.core import measurable
+from goats.core import axis
 from goats.core import metadata
-from goats.core import metric
+from goats.core import observed
+from goats.core import parameter
+from goats.core import variable
 
 
-# Goal: Create classes with zero or more metadata attributes, to which they can
-# add their own data value(s) or callable. Not all classes will inherit from
-# `metadata.Quantified`. For example, an observable quantity takes a callable
-# implementation object with which it creates an observed quantity.
+class Implementation(abc.ABC):
+    """Base class for observable implementations."""
 
-class Quantity:
-    """An observable quantity."""
+    Arguments = typing.TypeVar('Arguments', bound=typing.Mapping)
+    Arguments = typing.Mapping[
+        str,
+        typing.Union[parameter.Assumption, parameter.Option]
+    ]
+
+    def __init__(
+        self,
+        name: str,
+        variables: variable.Interface,
+        axes: axis.Interface,
+        arguments: Arguments,
+    ) -> None:
+        self.name = name
+        self.variables = variables
+        self.axes = axes
+        self.arguments = arguments
+
+    @abc.abstractmethod
+    def apply(self, **constraints) -> observed.Quantity:
+        """Create an observation within the given constraints."""
+        pass
+
+    def get_dependency(self, name: str):
+        """Get a named dependency of this observable."""
+        if name in self.arguments:
+            return self.arguments[name]
+
+    def get_variable(self, name: str, constraints: dict):
+        """Get the named variable from the dataset, if possible."""
+        interpolate = 'radius' in constraints
+        # Port code from `eprem.observables.Application`.
+        if variable := self.variables.get(name):
+            return self.axes.subscript(variable)
+
+
+class Primary(Implementation):
+    """A primary observable."""
+
+    def apply(self, **constraints):
+        return self.get_variable(self.name, constraints)
+
+
+class Derived(Implementation):
+    """A derived observable."""
+
+    def apply(self, **constraints):
+        variable = self._evaluate(self.name, *args, **kwargs)
+
+    def _evaluate(self, name: str, *args, **kwargs):
+        """Evaluate the named observable."""
+        if name in self.dataset:
+            return
+
+
+class Compound(Implementation):
+    """A compound observable."""
+
+    def apply(self, **constraints):
+        expression = algebraic.Expression(self.name)
+
+
+class Metadata(
+    metadata.UnitMixin,
+    metadata.NameMixin,
+    metadata.AxesMixin,
+): ...
+
+class Quantity(Metadata):
+    """A quantity that produces an observation."""
 
     def __init__(
         self,
@@ -26,12 +91,55 @@ class Quantity:
         name: typing.Union[str, typing.Iterable[str], metadata.Name]=None,
         axes: typing.Union[str, typing.Iterable[str], metadata.Axes]=None,
     ) -> None:
+        self.interface = __implementation
         self._unit = unit
         self._name = name
         self._axes = axes
-        super().__init__(__implementation)
+        self._constraints = None
 
-    def apply_conversion(self, new: metric.Unit):
-        self._unit = new
+    def observe(self, update: bool=False, **constraints) -> observed.Quantity:
+        """Create an observation within the given constraints.
+        
+        This method will create a new observation of this observable quantity by
+        applying `constraints`, if given, or the default constraints. The
+        default collection of observational constraints uses all relevant axis
+        indices and default parameter values.
+
+        Parameters
+        ----------
+        update : bool, default=False
+            If true, update the existing constraints from the given constraints.
+            The default behavior is to use only the given constraints.
+
+        **constraints
+            Key-value pairs of axes or parameters to update.
+
+        Returns
+        -------
+        `~observed.Quantity`
+            An object representing the resultant observation.
+        """
+        if update:
+            self._constraints.update(constraints)
+        else:
+            self._constraints = constraints or {}
+
+
+class Interface(collections.abc.Mapping):
+    """ABC for interfaces to observable quantities."""
+
+    def __init__(self, *available: str) -> None:
+        self.available = available
+        """The names of all observable quantities."""
+
+    def __len__(self) -> int:
+        return len(self.available)
+
+    def __iter__(self) -> typing.Iterator:
+        return iter(self.available)
+
+    def __getitem__(self, __k: str) -> Quantity:
+        """Get the named observable quantity."""
+        raise NotImplementedError
 
 
