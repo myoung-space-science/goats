780945da392651b8f1b5a1291526050f6ac9f3d0
Matthew Young <matthew.young@unh.edu>
2022-07-21 17:01:25 -0400

    Rename `assumptions` to `scalars`

--- src/goats/core/observed.py
+++ src/goats/core/observed.py
@@ -15,19 +15,19 @@ class Quantity(variable.Quantity):
         self,
         __v: variable.Quantity,
         indices: typing.Mapping[str, index.Quantity],
-        assumptions: typing.Mapping[str, parameter.Assumption]
+        scalars: typing.Mapping[str, parameter.Assumption]
     ) -> None:
         self._result = __v
         self._indices = indices
-        self._assumptions = assumptions
-        # This should provide an aliased access to indices and assumptions. In
+        self._scalars = scalars
+        # This should provide an aliased access to indices and scalars. In
         # practice, they have be aliased mappings themselves, but that may not
         # be true in general.
 
     def __getitem__(self, __k):
-        """Get an assumption by name or array values by index."""
-        if __k in self._assumptions:
-            return self._assumptions[__k]
+        """Get a scalar by name or array values by index."""
+        if __k in self._scalars:
+            return self._scalars[__k]
         if __k in self._indices:
             return self._indices[__k]
         return self._result[__k] # catch `IndexError` as `KeyError`?
@@ -44,23 +44,19 @@ class Quantity(variable.Quantity):
 
     @property
     def parameters(self):
-        """The names of assumptions relevant to this observation."""
+        """The names of scalar assumptions relevant to this observation."""
         if self._parameters is None:
-            if isinstance(self._assumptions, aliased.Mapping):
-                self._parameters = self._assumptions.keys(aliased=True)
+            if isinstance(self._scalars, aliased.Mapping):
+                self._parameters = self._scalars.keys(aliased=True)
             else:
-                self._parameters = self._assumptions.keys()
+                self._parameters = self._scalars.keys()
         return self._parameters
-
-    def __array__(self, *args, **kwargs) -> numpy.ndarray:
-        """Support automatic conversion to a `numpy.ndarray`."""
-        return self._data.__array__(*args, **kwargs)
 
     def __eq__(self, other) -> bool:
         """True if two instances have equivalent attributes."""
         if not isinstance(other, type(self)):
             return NotImplemented
-        if not self._equal_attrs(other, 'indices', 'assumptions'):
+        if not self._equal_attrs(other, 'indices', 'scalars'):
             return False
         return super().__eq__(other)
 
