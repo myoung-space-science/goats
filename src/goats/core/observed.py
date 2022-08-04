b7e1df6044fb93f601273c346a97f66c7a0a1182
Matthew Young <matthew.young@unh.edu>
2022-08-04 14:19:25 -0400

    Refactor Quantity to make use of parent class

--- src/goats/core/observed.py
+++ src/goats/core/observed.py
@@ -1,10 +1,5 @@
 import typing
 
-import numpy
-
-from goats.core import aliased
-from goats.core import constant
-from goats.core import index
 from goats.core import variable
 
 
@@ -14,49 +9,31 @@ class Quantity(variable.Quantity):
     def __init__(
         self,
         __v: variable.Quantity,
-        indices: typing.Mapping[str, index.Quantity],
-        scalars: typing.Mapping[str, constant.Assumption]
+        context: typing.Mapping,
     ) -> None:
-        self._result = __v
-        self._indices = indices
-        self._scalars = scalars
-        # This should provide an aliased access to indices and scalars. In
-        # practice, they have be aliased mappings themselves, but that may not
-        # be true in general.
+        super().__init__(__v)
+        self._indices = {
+            k: v for k, v in context.items()
+            if k in self.axes
+        }
+        self._scalars = {
+            k: v for k, v in context.items()
+            if k not in self.axes
+        }
+        self.parameters = list(self._scalars)
+        """The names of scalar assumptions relevant to this observation."""
 
     def __getitem__(self, __k):
         """Get a scalar by name or array values by index."""
         if __k in self._scalars:
             return self._scalars[__k]
-        if __k in self._indices:
-            return self._indices[__k]
-        return self._result[__k] # catch `IndexError` as `KeyError`?
-
-    @property
-    def axes(self):
-        """The indexable axes of this observation's array."""
-        if self._axes is None:
-            if isinstance(self._indices, aliased.Mapping):
-                self._axes = self._indices.keys(aliased=True)
-            else:
-                self._axes = self._indices.keys()
-        return self._axes
-
-    @property
-    def parameters(self):
-        """The names of scalar assumptions relevant to this observation."""
-        if self._parameters is None:
-            if isinstance(self._scalars, aliased.Mapping):
-                self._parameters = self._scalars.keys(aliased=True)
-            else:
-                self._parameters = self._scalars.keys()
-        return self._parameters
+        return super().__getitem__(__k)
 
     def __eq__(self, other) -> bool:
         """True if two instances have equivalent attributes."""
         if not isinstance(other, type(self)):
             return NotImplemented
-        if not self._equal_attrs(other, 'indices', 'scalars'):
+        if not self._equal_attrs(other, '_indices', '_scalars'):
             return False
         return super().__eq__(other)
 
