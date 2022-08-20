df672c5c167440ca30a14aa607230a7c02f494b8
Matthew Young <matthew.young@unh.edu>
2022-08-20 11:21:35 -0400

    Refactor/fix bugs in observed quantity

--- src/goats/core/observed.py
+++ src/goats/core/observed.py
@@ -3,10 +3,11 @@ import typing
 import numpy
 
 from goats.core import index
+from goats.core import iterables
 from goats.core import variable
 
 
-class Quantity:
+class Quantity(iterables.ReprStrMixin):
     """The result of observing an observable quantity."""
 
     def __init__(
@@ -15,17 +16,42 @@ class Quantity:
         indices: typing.Mapping[str, index.Quantity],
         **assumptions
     ) -> None:
-        self.data = numpy.array(__v.data)
-        """The array of this observation's data."""
-        self.unit = property(__v.unit)
-        """The metric unit of this observation's data values."""
-        self.name = property(__v.name)
-        """The name(s) of this observation."""
-        self.dimensions = property(__v.axes)
-        """The names of axes in this observation's data array."""
+        self._quantity = __v
         self._indices = indices
         self._assumptions = assumptions
+        self._data = None
+        self._unit = None
+        self._aliases = None
+        self._dimensions = None
         self._parameters = None
+
+    @property
+    def data(self):
+        """The array of this observation's data."""
+        if self._data is None:
+            self._data = numpy.array(self._quantity.data)
+        return self._data
+
+    @property
+    def unit(self):
+        """The metric unit of this observation's data values."""
+        if self._unit is None:
+            self._unit = self._quantity.unit
+        return self._unit
+
+    @property
+    def aliases(self):
+        """The name(s) of this observation."""
+        if self._aliases is None:
+            self._aliases = self._quantity.name
+        return self._aliases
+
+    @property
+    def dimensions(self):
+        """The names of axes in this observation's data array."""
+        if self._dimensions is None:
+            self._dimensions = self._quantity.axes
+        return self._dimensions
 
     @property
     def parameters(self):
@@ -47,7 +73,7 @@ class Quantity:
         """True if two instances have equivalent attributes."""
         if not isinstance(other, type(self)):
             return NotImplemented
-        if not self._equal_attrs(other, '_indices', '_scalars'):
+        if not self._equal_attrs(other, '_indices', '_assumptions'):
             return False
         return super().__eq__(other)
 
@@ -65,5 +91,5 @@ class Quantity:
             f"dimensions={self.dimensions}",
             f"parameters={self.parameters}",
         ]
-        return f"'{self.name}': {', '.join(attrs)}"
+        return f"'{self.aliases}': {', '.join(attrs)}"
 
