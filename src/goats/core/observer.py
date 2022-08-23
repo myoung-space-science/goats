928f207eb50ca79b8ab700d7c2941427bcbe138a
Matthew Young <matthew.young@unh.edu>
2022-08-23 12:26:49 -0400

    Observer Interface:
    * Initialize with names of unobservable quantities
    * Initialize with optional name of metric system
    * Allow updates to data source to generate new context and interface
    * Inherit from abc.ABC (required for `process`)
    * Define base `readfrom` method (not abstract)
    * Define `source` property
    * Define `observables` property
    * Define `context` property
    * Define `assumptions` property via observing interface
    * Define `names` property
    * Define `data` property (pseudo-abstract)
    * Refactor __getitem__

--- src/goats/core/observer.py
+++ src/goats/core/observer.py
@@ -6,6 +6,7 @@ from goats.core import algebraic
 from goats.core import computable
 from goats.core import index
 from goats.core import metadata
+from goats.core import metric
 from goats.core import observable
 from goats.core import observed
 from goats.core import observing
@@ -15,54 +16,110 @@ from goats.core import variable
 from goats.core import spelling
 
 
-class Interface:
+class Interface(abc.ABC):
     """The base class for all observers."""
+
+    _data: observing.Interface=None
 
     def __init__(
         self,
-        data: observing.Interface,
-        names: typing.Iterable[str],
-        *others: typing.Mapping[str, typing.Any]
+        *unobservable: str,
+        system: str='mks',
     ) -> None:
         """Initialize this instance.
         
         Parameters
         ----------
-        data : `~observing.Interface`
-            The interface to variable, axis, and constant quantities available
-            to this observer.
+        *unobservable : string
+            The names of variable quantities from this observer's dataset to
+            exclude from the set of formally observable quantities. These
+            quantities will still be accessible in their variable form.
 
-        names : iterable of string
-            The names of formally observable quantities.
-
-        *others : mappings
-            Zero or more mappings from string key to any object that users
-            should be able to access from this observer. All included quantities
-            will be available to users via the standard bracket syntax (i.e.,
-            `observer[<key>]`).
+        system : string, default='mks'
+            The metric system to use for variable and observable quantities.
         """
-        self.data = data
-        self.context = observing.Context(self.data)
-        self.observables = observable.Interface(
-            data,
-            *names,
-            context=self.context,
-        )
-        self.others = others
-        keys = list(self.observables)
-        keys.append([key for mapping in others for key in mapping.keys()])
-        self._spellcheck = spelling.SpellChecker(keys)
+        self._unobservable = unobservable
+        self._system = metric.System(system)
+        self._source = None
+        self._context = None
+        self._observables = None
+        self._spellcheck = None
+
+    def system(self, new: str=None):
+        """Get or set this observer's metric system."""
+        if not new:
+            return self._system
+        self._system = metric.System(new)
+        return self
+
+    def readfrom(self, source):
+        """Update this observer's data source."""
+        self._source = source
+        self._context = None
+        self._observables = None
+        return self
+
+    @property
+    def source(self):
+        """The source of this observer's data."""
+        return self._source
+
+    @property
+    def observables(self):
+        """An interface to formally observable quantities."""
+        if self._observables is None:
+            self._observables = observable.Interface(
+                self.data,
+                *self.names,
+                context=self.context,
+            )
+        return self._observables
+
+    @property
+    def context(self):
+        """This observer's observing context."""
+        if self._context is None:
+            self._context = observing.Context(self.data)
+        return self._context
+
+    @property
+    def assumptions(self):
+        """The operational assumptions available to this observer."""
+        return self.data.assumptions
+
+    @property
+    def names(self):
+        """The names of formally observable quantities."""
+        fromdata = list(self.data.variables) + list(self.data.functions)
+        available = set(fromdata) - set(self._unobservable)
+        return tuple(available)
+
+    @property
+    def data(self):
+        """An interface to this observer's dataset quantities."""
+        if self._data is None:
+            raise NotImplementedError(
+                f"Observer requires an observing interface"
+            ) from None
+        return self._data
 
     def __getitem__(self, key: str):
         """Access an observable quantity by keyword, if possible."""
         if key in self.observables:
             return self.observables[key]
-        for mapping in self.others:
-            if key in mapping:
-                return mapping[key]
-        if self._spellcheck.misspelled(key):
-            raise spelling.SpellingError(key, self._spellcheck.suggestions)
+        if key in self.assumptions:
+            return self.assumptions[key]
+        self._check_spelling(key) # -> None if `key` is spelled correctly
         raise KeyError(f"No observable for {key!r}") from None
+
+    def _check_spelling(self, key: str):
+        """Catch misspelled names of observable quantities, if possible."""
+        keys = list(self.observables) + list(self.assumptions)
+        if self._spellcheck is None:
+            self._spellcheck = spelling.SpellChecker(*keys)
+        else:
+            self._spellcheck.words |= keys
+        return self._spellcheck.check(key)
 
     def observe(
         self,
