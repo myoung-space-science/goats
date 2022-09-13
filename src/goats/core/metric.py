0ae2b2bd5c425ec39370c581a5b3a29f4be80d8a
Matthew Young <matthew.young@unh.edu>
2022-09-13 12:01:23 -0400

    Edit Quantity docstrings

--- src/goats/core/metric.py
+++ src/goats/core/metric.py
@@ -2601,6 +2601,10 @@ class Quantity(iterables.ReprStrMixin):
         triviall equal, or they represent distinct quantities, in which they are
         not equal. In addition, an instance of this class will compare equal to
         its case-insensitive name.
+
+        See Also
+        --------
+        ``~metric.Quantity.__or__`` : test equivalence between quantities.
         """
         if isinstance(other, str):
             return other.lower() == self.name
@@ -2609,11 +2613,12 @@ class Quantity(iterables.ReprStrMixin):
     def __or__(self, other) -> bool:
         """Called to test equivalence via self | other.
         
-        Two metric quantities are equivalent if their dimensions are equal. This
-        operation thus provides a way to compare unequal quantities to determine
-        if they are have a physical correspondence. For example, energy and work
-        are distinct quantities, but they have identical dimensions and are
-        linked through the work-energy theorem.
+        Two metric quantities are equivalent if their dimensions are equal in a
+        given metric system. This operation thus provides a way to compare
+        unequal quantities to determine if they are have a physical
+        correspondence. For example, energy and work are distinct quantities,
+        but they have identical dimensions and are linked through the
+        work-energy theorem.
 
         This operation is only defined between two instances of this class.
         """
