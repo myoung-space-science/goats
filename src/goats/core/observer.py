2475858e8ccdbc347f27ee770eeba01ef0210da2
Matthew Young <matthew.young@unh.edu>
2022-09-21 13:44:45 -0400

    Remove deprecated observing methods:
    * observe
    * _observe
    * evaluate
    * process
    * compute
    * get_dependency
    * get_observable

--- src/goats/core/observer.py
+++ src/goats/core/observer.py
@@ -13,7 +13,7 @@ from goats.core import symbolic
 from goats.core import variable
 
 
-class Interface(abc.ABC):
+class Interface:
     """The base class for all observers."""
 
     _data: observing.Interface=None
@@ -161,102 +161,4 @@ class Interface(abc.ABC):
         else:
             self._spellcheck.words |= keys
         return self._spellcheck.check(key)
-
-    # 2022-09-20: If the current reorganization effort succeeds, the following
-    # methods should be obsolete
-    # - observe
-    # - _observe
-    # - evaluate
-    # - process
-    # - compute
-    # - get_dependency
-    # - get_observable
-    def observe(
-        self,
-        quantity: typing.Union[str, metadata.Name, observable.Quantity],
-        **constraints
-    ) -> observed.Quantity:
-        """Create an observation within the given constraints.
-        
-        This method will create a new observation of the target observable
-        quantity after applying the given constraints. The default collection of
-        observational constraints uses all relevant axis indices and default
-        parameter values.
-
-        Parameters
-        ----------
-        quantity : string, `~metadata.Name`, or `~observable.Quantity`
-            The quantity to observe. If the argument is a string or instance of
-            `~metadata.Name`, this method will first retrieve the corresponding
-            `~observable.Quantity`. Users may also pass an existing instance of
-            `~observable.Quantity` with updated unit or predefined constraints.
-
-        **constraints
-            Key-value pairs of axes or parameters to update. These constraints
-            supercede constraints defined on `quantity` if it is an instance of
-            `~observable.Quantity`.
-
-        Returns
-        -------
-        `~observed.Quantity`
-            An object representing the resultant observation.
-        """
-        target = (
-            self.interface[quantity] if isinstance(quantity, str)
-            else quantity
-        )
-        self.context.apply(**constraints)
-        result = self._observe(target.name)
-        indices = {k: self.context.get_index(k) for k in result.axes}
-        scalars = {k: self.context.get_value(k) for k in result.parameters}
-        return observed.Quantity(result[target.unit], indices, **scalars)
-
-    def _observe(self, name: metadata.Name) -> observing.Quantity:
-        """Internal helper for `~Interface.observe`."""
-        s = list(name)[0]
-        expression = symbolic.Expression(reference.NAMES.get(s, s))
-        term = expression[0]
-        result = self.get_observable(term.base)
-        if len(expression) == 1:
-            # We don't need to multiply or divide quantities.
-            if term.exponent == 1:
-                # We don't even need to raise this quantity to a power.
-                return result
-            return result ** term.exponent
-        q0 = result ** term.exponent
-        if len(expression) > 1:
-            for term in expression[1:]:
-                result = self.get_observable(term.base)
-                q0 *= result ** term.exponent
-        return q0
-
-    def evaluate(self, q) -> observing.Quantity:
-        """Create an observing result based on this quantity."""
-        if isinstance(q, computed.Quantity):
-            parameters = [p for p in q.parameters if p in self.data.constants]
-            return observing.Quantity(self.compute(q), parameters=parameters)
-        if isinstance(q, variable.Quantity):
-            return observing.Quantity(self.process(q))
-        raise ValueError(f"Unknown quantity: {q!r}") from None
-
-    @abc.abstractmethod
-    def process(self, name: str) -> variable.Quantity:
-        """Compute observer-specific updates to a variable quantity."""
-        raise NotImplementedError
-
-    def compute(self, q: computed.Quantity):
-        """Determine dependencies and compute the result of this function."""
-        dependencies = {p: self.get_dependency(p) for p in q.parameters}
-        return q(**dependencies)
-
-    def get_dependency(self, key: str):
-        """Get the named constant or variable quantity."""
-        if this := self.get_observable(key):
-            return this
-        return self.context.get_value(key)
-
-    def get_observable(self, key: str):
-        """Retrieve and evaluate an observable quantity."""
-        if quantity := self.data.get_observable(key):
-            return self.evaluate(quantity)
 
