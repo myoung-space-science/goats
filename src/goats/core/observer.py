2e2a040c40ef9c47e3f61738a06e2999e8a9186b
Matthew Young <matthew.young@unh.edu>
2022-11-21 17:16:28 -0500

    WIP on new observing framework:
    * Redefine Interface in terms of new objects

--- src/goats/core/observer.py
+++ src/goats/core/observer.py
@@ -5,7 +5,6 @@ from goats.core import metadata
 from goats.core import metric
 from goats.core import observable
 from goats.core import observing
-from goats.core import reference
 from goats.core import spelling
 from goats.core import symbolic
 
@@ -15,18 +14,13 @@ class Interface:
 
     def __init__(
         self,
-        __data: observing.Dataset,
         *unobservable: str,
         system: str='mks',
-        apply: typing.Type[observing.Application]=None,
     ) -> None:
         """Initialize this instance.
         
         Parameters
         ----------
-        __data : `~observing.Dataset`
-            The observer's dataset.
-
         *unobservable : string
             The names of variable quantities from this observer's dataset to
             exclude from the set of formally observable quantities. These
@@ -34,41 +28,16 @@ class Interface:
 
         system : string, default='mks'
             The metric system to use for variable and observable quantities.
-
-        apply : type of observing application, optional
-            A subclass of `~observing.Application` with which to evaluate
-            operational parameters and variable quantities during the observing
-            process. If `application` is absent, this class will use an instance
-            of the base class.
         """
-        self._data = __data
         self._unobservable = unobservable
         self._system = metric.System(system)
-        self._application_type = apply or observing.Application
         self._quantities = None
-        self._application = None
         self._spellcheck = None
 
-    def reset(self, source=None, **kwargs):
-        """Reset the dataset interface.
-        
-        The base implementation resets various data-related attributes to their
-        uninitialized values and sets `source`, if given, as the new target from
-        which to read data. Concrete subclasses may wish to implement additional
-        data-related logic (e.g., reinitialize interfaces), possibly after
-        modifying `source` (e.g., to normalize a path).
-
-        Parameters
-        ----------
-        source, optional
-            The new source of this observer's data. The acceptable type(s) will
-            be observer-specific.
-        """
-        self._quantities = None
-        self._application = None
+    def update(self, __interface: observing.Interface):
+        """Use a new interface to physical quantities."""
+        self._quantities = __interface
         self._spellcheck = None
-        if source is not None:
-            self._data.readfrom(source, **kwargs)
         return self
 
     @property
@@ -77,100 +46,62 @@ class Interface:
         return self._system
 
     @property
-    def application(self):
-        """This observer's observing application."""
-        if self._application is None:
-            self._application = self._application_type(self.quantities)
-        return self._application
-
-    @property
-    def parameters(self):
-        """The names of operational arguments relevant to this observer."""
-        keys = self.quantities.constants.keys(aliased=True)
-        return aliased.KeyMap(*keys)
-
-    @property
     def observables(self):
         """The names of formally observable quantities.
         
-        This property contains the names of all primary and derived observable
-        quantities. See `~observing.Interface.primary` and
-        `~observing.Interface.derived` for their respective definitions. Names
-        are listed as groups of aliases for each observable quantity (e.g., 'mfp
-        | mean free path | mean_free_path'); any of the listed aliases is a
-        valid key for that quantity.
+        This property contains the names of physical quantities from which this
+        observer can create an observation. Names are listed as groups of
+        aliases for each observable quantity (e.g., 'mfp | mean free path |
+        mean_free_path'); any of the listed aliases is a valid key for that
+        quantity.
 
         Note that it is also possible to symbolically compose new observable
         quantities from those listed here (e.g., 'mfp / Vr'). Therefore, this
         collection represents the minimal set of quantities that this observer
         can observe.
         """
-        primary = self.quantities.primary
-        derived = self.quantities.derived
-        available = aliased.KeyMap(*primary, *derived)
+        available = aliased.KeyMap(self.quantities.observables)
         return available.without(*self._unobservable)
 
     @property
     def quantities(self):
         """An interface to this observer's physical quantities.
         
-        This property represents the variable, axis-indexing, and constant
-        quantities that are available from this observer's dataset. It
-        incorporates this observer's metric system, and exposes objects that
-        support arithmetic operations and conversion to numpy arrays.
+        This property represents the quantities that this observer will use when
+        making observations.
         """
-        if self._quantities is None:
-            self._quantities = observing.Quantities(
-                self._data,
-                system=self.system,
-            )
         return self._quantities
 
-    def __getitem__(self, key: str):
+    def __getitem__(self, __k: str):
         """Access an observable quantity by keyword, if possible."""
-        if q := self._get_quantity(key):
-            return q
-        self._check_spelling(key) # -> None if `key` is spelled correctly
-        raise KeyError(f"No observable for {key!r}") from None
+        if self.observes(__k):
+            return observable.Quantity(self.quantities, __k)
+        if __k in self.quantities:
+            return self.quantities[__k]
+        self._check_spelling(__k) # -> None if `__k` is spelled correctly
+        raise KeyError(f"No observable for {__k!r}") from None
 
-    def _get_quantity(self, key: str):
-        """Retrieve the named quantity from an interface, if possible."""
-        if self.knows(key):
-            name = metadata.Name(
-                key if observable.iscomposed(key)
-                else reference.ALIASES[key]
-            )
-            return observable.Quantity(
-                name,
-                observing.Implementation(key, self.quantities),
-                application=self.application,
-            )
-        if key in self.quantities.variables:
-            return self.quantities.variables[key]
-        if key in self.quantities.constants:
-            return self.quantities.constants[key]
-
-    def knows(
+    def observes(
         self,
         name: typing.Union[str, typing.Iterable[str], metadata.Name],
     ) -> bool:
         """True if this interface can observe the named quantity."""
         if isinstance(name, str):
-            return self._knows(name)
-        return next((self._knows(key) for key in name), False)
+            return self._observes(name)
+        return next((self._observes(key) for key in name), False)
 
-    def _knows(self, key: str):
-        """Internal helper for `~Interface.knows`."""
+    def _observes(self, key: str):
+        """Internal helper for `~Interface.observes`."""
         if key in list(self.observables):
             return True
         if observable.iscomposed(key):
             expression = symbolic.Expression(key)
-            return all(self._knows(term.base) for term in expression)
+            return all(self._observes(term.base) for term in expression)
         return False
 
     def _check_spelling(self, key: str):
-        """Catch misspelled names of observable quantities, if possible."""
-        keys = list(self.observables) + list(self.parameters)
+        """Catch misspelled names of physical quantities, if possible."""
+        keys = list(self.quantities)
         if self._spellcheck is None:
             self._spellcheck = spelling.SpellChecker(*keys)
         else:
